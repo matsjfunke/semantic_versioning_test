b70dd0ef2f8ace3952643368fa9ce4e67990fd51
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2025-05-02 18:55:53 +0200

    feat: release should work

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,6 +20,9 @@ module.exports = {
     "footer-max-line-length": [2, "always", 200],
     "body-leading-blank": [2, "always"],
     "header-max-length": [2, "always", 200],
+    "subject-case": [0, "always"],
+    "subject-full-stop": [0, "never"],
+    "type-case": [2, "always", "lower-case"],
   },
   helpUrl:
     "https://github.com/matsjfunke/halunken-website/blob/main/commitlint.config.js",
