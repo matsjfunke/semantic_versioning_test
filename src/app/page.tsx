fb83d14d9138828fc01b5ad7768ce94412d0901c
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2025-05-05 14:53:04 +0200

    feat: added some text to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Semantic Versioning Test</h1>
+      <p>
+        This is a test of semantic versioning.
+      </p>
     </div>
   );
 }
