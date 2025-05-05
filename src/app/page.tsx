c881042b580c075ebb6383bd8800c850dbac2b29
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2025-05-05 14:29:39 +0200

    fix: error gone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Semantic Versioning Test</h1>
     </div>
   );
