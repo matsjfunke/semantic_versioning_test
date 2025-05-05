d93088ffe7d2151e58d177a3e4296411b0565706
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2025-05-05 19:54:43 +0200

    feat: added setVersion button to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  const [version, setVersion] = useState(0);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Semantic Versioning Test</h1>
-      <p>
-        This is a test of semantic versioning.
-      </p>
+      <p>This is a test of semantic versioning.</p>
+      <button
+        onClick={() => {
+          setVersion(version + 1);
+        }}
+      >
+        Click me
+      </button>
+      <p>Version: {version}</p>
     </div>
   );
 }
